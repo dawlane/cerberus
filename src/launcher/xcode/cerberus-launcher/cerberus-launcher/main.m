8806c37f7586d567052bdaf0dac9b08a0aef48d9
dawlane <dawlane@hotmail.com>
2018-10-14 18:06:46 +0100

    Fix to Cerberus launcher for Mac OSX
    Previously use Ted's bundle id, but would launch wrong version of Ted if there was more than one version of Cerberus installed.

--- src/launcher/xcode/cerberus-launcher/cerberus-launcher/main.m
+++ src/launcher/xcode/cerberus-launcher/cerberus-launcher/main.m
@@ -3,9 +3,10 @@
 //
 
 #include <Foundation/Foundation.h>
+#include <CoreServices/CoreServices.h>
 #include <AppKit/AppKit.h>
 
-#define APP_BUNDLE_ID "com.krautapps.Ted"
+//#define APP_BUNDLE_ID "com.krautapps.Ted"
 
 void outlist(NSMutableArray *array){
     for(int i = 1;i<array.count-1;i++){
@@ -14,25 +15,33 @@ void outlist(NSMutableArray *array){
 }
 
 int main(int argc, const char * argv[]) {
-    NSMutableArray *files = [[NSMutableArray alloc] initWithCapacity:0];
+    NSMutableArray *files = [[NSMutableArray alloc] initWithCapacity:0]; // Array for file list
     
+    NSBundle *main = [NSBundle mainBundle]; // This should give us the path to this running instance
+    
+    // String hack
+    NSString *targetPath = [main.bundleURL.absoluteString stringByReplacingOccurrencesOfString:@"Cerberus.app" withString:@"bin/Ted.app"];
+    NSURL *targetURL = [NSURL URLWithString:targetPath];
+    
+    // Process the arguments. Caution anything can be processed.
     for(int i = 0;i<argc;i++){
         NSString *str = [NSString stringWithUTF8String:argv[i]];
         [files addObject:[NSURL URLWithString:str]];
     }
     
+    // Use NSWorkspace and pass the URL's to Ted
     if(![[NSWorkspace sharedWorkspace] openURLs:files
-                        withAppBundleIdentifier:@APP_BUNDLE_ID
+                           withApplicationAtURL:targetURL
                                         options:NSWorkspaceLaunchDefault
-                 additionalEventParamDescriptor:nil
-                              launchIdentifiers:nil])
+                                  configuration:nil
+                                          error:nil])
     {
-        
         NSLog(@"ERROR");
         outlist(files);
     } else {
-        NSLog(@"%@", [[NSWorkspace sharedWorkspace] absolutePathForAppBundleWithIdentifier:@APP_BUNDLE_ID]);
+        NSLog(@"%@", targetURL);
         outlist(files);
     }
+    
     return 0;
 }
